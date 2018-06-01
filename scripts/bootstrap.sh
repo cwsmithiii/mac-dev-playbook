260429a3a3c3d343953bce98a2464ebf194032b5
Charles Smith <csmith@palantir.com>
2018-05-31 17:33:04 -0700

    Addition of environment support.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -3,17 +3,30 @@
 
 # Get directory of this script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT_DIR="$( dirname "${DIR}" )"
+CONFIG_DIR="${ROOT_DIR}/ansible/configs"
+
+# Confirm environment argument defined
+if [ $# -eq 0 ]; then
+  echo "This script, '$0', requires that an environment be given."
+  exit 1
+else
+  ENVIRONMENT=$1
+fi
 
 # Load colors used
 RESTORE=$(echo -en '\033[0m')
 GREEN=$(echo -en '\033[00;32m')
 
 # Load all common functions / variables
+source "${DIR}/common/environment.sh"
 source "${DIR}/common/sudo.sh"
 source "${DIR}/common/brew.sh"
 source "${DIR}/common/ansible.sh"
 
 # Run functions
+environment_cleanup "${CONFIG_DIR}"
+environment_link "${CONFIG_DIR}" "${ENVIRONMENT}"
 ask_sudo
 pre_install_brew
 install_brew
---
+++ scripts/common/environment.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+environment_cleanup() {
+  if [ $# -ne 1 ]; then
+    echo "The configuration directory must be given."
+    exit 1
+  else
+    local DIR=$1
+  fi
+
+  echo "Removing existing environment configs."
+  for file in ${DIR}/*.yml; do
+    echo "  Removing ${file}."
+    rm -r "${file}"
+  done
+}
+
+environment_link() {
+  if [ $# -ne 2 ]; then
+    echo "The configuration directory and environment must be given."
+    exit 1
+  else
+    local DIR=$1
+    local ENVIRONMENT=$2
+  fi
+
+  if [ "${ENVIRONMENT}" != "cleanup" ]; then
+    if [ -d "${DIR}/${ENVIRONMENT}" ]; then 
+      echo "Linking ${ENVIRONMENT} files."
+      for file in ${DIR}/${ENVIRONMENT}/*.yml; do
+        echo "  Linking ${file}."
+        ln -s "${file}" "${DIR}"
+      done
+    else
+      echo "${ENVIRONMENT} doesn't exist."
+      exit 1
+    fi
+  fi
+}
